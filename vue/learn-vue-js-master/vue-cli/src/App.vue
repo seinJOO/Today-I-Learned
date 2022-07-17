3d9981185c101a09aef55c3ab241a75a29d4d3c2
seinJOO <sein19950910@gmail.com>
2022-07-17 13:05:32 +0900

    event emit 실습
    하위 컴포넌트의 이벤트 → 상위 컴포넌트 메서드 실행 & props 변경 → 하위컴포넌트가 변경된데이터 받기

--- vue/learn-vue-js-master/vue-cli/src/App.vue
+++ vue/learn-vue-js-master/vue-cli/src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
-    <app-header v-bind:propsdata="str"></app-header>  
     <!-- 프롭스 속성 이름은 해당 하위 컴포넌트(app-header = AppHeader.vue)에 정의된 props 변수
           상위 컴포넌트인 App.vue에서 하위 컴포넌트에 str값을 내려줌 -->
+    <app-header v-bind:propsdata="str" v-on:renew="renewStr"></app-header>  
+    <!-- v-on:하위컴포넌트 이벤트명="상위컴포넌트의 메서드" 
+         AppHeader로부터 renew 이벤트를 받으면 renewStr 메서드 실행해서 str 변경 > propsdata 변경 -->
   </div>
 </template>
 <script>
@@ -16,6 +18,11 @@ export default {
   },
   components: {
     'app-header': AppHeader // 2. 컴포넌트 등록
+  },
+  methods: {
+    renewStr: function() {
+      this.str = 'Hi~~~';
+    }
   }
 }
 </script>
--- vue/learn-vue-js-master/vue-cli/src/components/AppHeader.vue
+++ vue/learn-vue-js-master/vue-cli/src/components/AppHeader.vue
@@ -2,12 +2,18 @@
   <header>
     <h1>{{ propsdata }}</h1>
     <!-- 상위 컴포넌트인 App.vue에서 전달받은 데이터(props) 표출 -->
+    <button v-on:click="sendEvent">send</button>
   </header>
 </template>
 
 <script>
 export default {
- props: ['propsdata']
+ props: ['propsdata'],
+ methods: {
+    sendEvent: function() {
+        this.$emit('renew');
+    }
+ }
 }
 </script>
 
