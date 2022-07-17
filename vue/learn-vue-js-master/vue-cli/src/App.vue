b662405484af5df101f07296e687ff9de30d7973
seinJOO <sein19950910@gmail.com>
2022-07-17 12:58:14 +0900

    props 실습
    상위 컴포넌트 → 하위 컴포넌트

--- vue/learn-vue-js-master/vue-cli/src/App.vue
+++ vue/learn-vue-js-master/vue-cli/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-    <app-header></app-header> <!-- 컴포넌트 사용 -->
+    <!-- <app-header v-bind:프롭스 속성 이름="상위 컴포넌트의 데이터 이름"></app-header> -->
+    <app-header v-bind:propsdata="str"></app-header>  
+    <!-- 프롭스 속성 이름은 해당 하위 컴포넌트(app-header = AppHeader.vue)에 정의된 props 변수
+          상위 컴포넌트인 App.vue에서 하위 컴포넌트에 str값을 내려줌 -->
   </div>
 </template>
 <script>
@@ -8,7 +11,7 @@ import AppHeader from './components/AppHeader.vue'  // 1. 컴포넌트 import
 export default {
   data: function() {  // .vue파일에서는 data 문법 꼭 지켜야함 ! 안그러면 콘솔에 오류
     return {
-      str: 'hi'
+      str: 'Header'
     }
   },
   components: {
--- vue/learn-vue-js-master/vue-cli/src/components/AppHeader.vue
+++ vue/learn-vue-js-master/vue-cli/src/components/AppHeader.vue
@@ -1,12 +1,13 @@
 <template>
   <header>
-    <h1>Header</h1>
+    <h1>{{ propsdata }}</h1>
+    <!-- 상위 컴포넌트인 App.vue에서 전달받은 데이터(props) 표출 -->
   </header>
 </template>
 
 <script>
 export default {
-
+ props: ['propsdata']
 }
 </script>
 
