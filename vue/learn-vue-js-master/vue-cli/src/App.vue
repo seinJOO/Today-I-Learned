704643d7f592b36fc5ffd1c31be9b4757c417995
seinJOO <sein19950910@gmail.com>
2022-07-17 12:39:59 +0900

    app.vue HelloWorld.vue
    기존에 배웠던 기초부분 간단하게 확인하기

---
+++ vue/learn-vue-js-master/vue-cli/src/App.vue
@@ -0,0 +1,32 @@
+ <template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- 컴포넌트 명명법 종류 -->
+    <!-- <hello-world></hello-world>
+    <HelloWorld></HelloWorld>
+    <HelloWorld/> -->
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld  // hello-world: HelloWorld => <hello-world></hello-world> 이 컴포넌트 사용법과 똑같음!
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
