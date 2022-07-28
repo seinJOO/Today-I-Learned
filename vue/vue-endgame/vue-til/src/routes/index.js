4f705e861fc40fe39aa102e16d6ab3172954c2df
seinJOO <94598267+seinJOO@users.noreply.github.com>
2022-07-28 12:59:34 +0900

    redirect

--- vue/vue-endgame/vue-til/src/routes/index.js
+++ vue/vue-endgame/vue-til/src/routes/index.js
@@ -10,6 +10,10 @@ export default new VueRouter({
   // routes에 의해 컨트롤되는 page component들 - views에 있음
   routes: [
     {
+      path: '/',
+      redirect: '/login', // 서버에 접속하면 가장 먼저 띄울 페이지로 redirect
+    },
+    {
       path: '/login',
       component: () => import('@/views/LoginPage.vue'),
     },
