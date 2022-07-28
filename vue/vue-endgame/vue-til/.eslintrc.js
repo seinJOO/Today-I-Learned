0f12719f150f0ee1a7fbf301c4eef33975b1bf67
seinJOO <94598267+seinJOO@users.noreply.github.com>
2022-07-28 12:51:04 +0900

    lecture init3

--- vue/vue-endgame/vue-til/.eslintrc.js
+++ vue/vue-endgame/vue-til/.eslintrc.js
@@ -1,36 +1,40 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  extends: ["plugin:vue/essential", "@vue/prettier"],
+  extends: ['plugin:vue/essential', '@vue/prettier'],
   rules: {
-    "no-console": "off",
+    'no-console': 'off',
     // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     // "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-    "prettier/prettier": ['error', {
-      singleQuote: true,
-      semi: true,
-      useTabs: false,
-      tabWidth: 2,
-      trailingComma: 'all',
-      printWidth: 80,
-      bracketSpacing: true,
-      arrowParens: 'avoid',
-    }]
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        semi: true,
+        useTabs: false,
+        tabWidth: 2,
+        trailingComma: 'all',
+        printWidth: 80,
+        bracketSpacing: true,
+        arrowParens: 'avoid',
+        endOfLine: 'auto',
+      },
+    ],
   },
   parserOptions: {
-    parser: "babel-eslint"
+    parser: '@babel/eslint-parser',
   },
   overrides: [
     {
       files: [
-        "**/__tests__/*.{j,t}s?(x)",
-        "**/tests/unit/**/*.spec.{j,t}s?(x)"
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
       ],
       env: {
-        jest: true
-      }
-    }
-  ]
+        jest: true,
+      },
+    },
+  ],
 };
