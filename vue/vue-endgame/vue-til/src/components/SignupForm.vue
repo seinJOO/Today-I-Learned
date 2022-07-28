d3586e8593f1d6ed92e324e18192d63c996481b8
seinJOO <94598267+seinJOO@users.noreply.github.com>
2022-07-28 15:08:46 +0900

    3-4 회원가입 폼 : async&await에 이어 부가적 설정 더해보기
    ES6 Destructuring, 백틱 드응로 데이터 처리 등

--- vue/vue-endgame/vue-til/src/components/SignupForm.vue
+++ vue/vue-endgame/vue-til/src/components/SignupForm.vue
@@ -15,9 +15,10 @@
         <label for="nickname"> NAME: </label>
         <input id="nickname" type="text" v-model="nickname" />
       </div>
-      <button type="submit">login</button>
+      <button type="submit">회원 가입</button>
       <!-- 버튼을 누르면 submit 이벤트가 이벤트 버블링에 의해 form으로 올라오게 되면서
             form에서는 v-on:submit을 통해 디렉티브 이벤트를 받아 submitForm을 실행함 -->
+      <p>{{ logMessage }}</p>
     </form>
   </div>
 </template>
@@ -31,20 +32,36 @@ export default {
     // 폼 태그의 각 데이터에 연결할 데이터 속성 지정 + 연결
     // => 마크업의 v-model에 바인딩된 동일한 이름의 값이 변경될 경우 vue에서 감지할 수 있음
     return {
+      // form values
       username: '',
       password: '',
       nickname: '',
+      // log
+      logMessage: '',
     };
   },
   methods: {
-    submitForm() {
-      console.log('폼 제출');
+    // async-await으로 비동기 처리 지정해주기
+    async submitForm() {
       const userData = {
         username: this.username,
         password: this.password,
         nickname: this.nickname,
       };
-      registerUser(userData);
+      //const response = await registerUser(userData);
+      // console.log(response.data); // 비동기 처리 결과 콘솔로 받아보기
+
+      const { data } = await registerUser(userData);
+      console.log(data.username); // 변수로 바로 꺼내서 볼 수 있음 (es6 destructuring)
+      this.logMessage = `${data.username}님이 가입되었습니다`;
+      // es6 백틱(`) => `${}` 으로 문자열 안에 바로 자바스크립트 변수값을 가져올 수 있음
+      this.initForm(); // 데이터 전송 후 폼에 입력된 데이터 비우기
+    },
+    initForm() {
+      this.username = '';
+      this.password = '';
+      this.nickname = ''; // 자바스크립트는 Type이 없는 언어이기 때문에 null로 설정해도 무관
+      // 단, 버그를 줄이기 위해서는 타입을 지정해줘도 좋음
     },
   },
 };
