cc2a021b266d7dbab6659b8c6db341453027c585
seinJOO <94598267+seinJOO@users.noreply.github.com>
2022-07-29 20:46:54 +0900

    4-1 로그인 구현 : api 추가, 폼 기본 마크업

--- vue/vue-endgame/vue-til/src/components/LoginForm.vue
+++ vue/vue-endgame/vue-til/src/components/LoginForm.vue
@@ -9,22 +9,40 @@
       <input id="password" type="text" v-model="password" />
     </div>
     <button type="submit">로그인</button>
+    <p>{{ logMessage }}</p>
   </form>
 </template>
 
 <script>
-import axios from 'axios';
+import { loginUser } from '@/api/index';
 
 export default {
   data() {
     return {
       username: '',
       password: '',
+      logMessage: '',
     };
   },
   methods: {
-    submitForm() {
-      axios.post();
+    async submitForm() {
+      const userData = {
+        username: this.username,
+        password: this.password,
+      };
+      //const response = await loginUser(userData);
+      //console.log(response);
+      // 콘솔에 출력 시 response는 정해진 형식으로 수신됨
+
+      // data 부분에 우리가 원하는 정보가 있음 => response의 data를 바로 꺼내기 {data}
+      const { data } = await loginUser(userData);
+      console.log(data.user.username);
+      this.logMessage = `${data.user.username}님 환영합니다`;
+      this.initForm();
+    },
+    initForm() {
+      this.username = '';
+      this.password = '';
     },
   },
 };
