ba83e8fd6f362152149a761ed4cbdda0206377fe
seinJOO <94598267+seinJOO@users.noreply.github.com>
2022-07-29 11:32:20 +0900

    env 파일 활용법
    env파일은 키=값으로 정의
    development <<< vue-cli-service serve
    test <<< vue-cli-service test:unit
    production <<< vue-cli-service build / vue-cli-service test:e2e
    https://cli.vuejs.org/guide/mode-and-env.html

--- vue/vue-endgame/vue-til/src/api/index.js
+++ vue/vue-endgame/vue-til/src/api/index.js
@@ -1,17 +1,11 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3000', // axios 인스턴스로 공통 설정을 담기
+  baseURL: process.env.VUE_APP_API_URL,
 });
 
 function registerUser(userData) {
   return instance.post('signup', userData);
-
-  // const url = 'http://localhost:3000/signup';
-  //return axios.post(url, userData);
-  //   (method) Axios.post (url: string, data?: any, config?: AxiosRequestConfig<any>)
-  //    post()의 result는 Promise<AxiosResponse<any, any>>
 }
 
 export { registerUser };
-// 여기서 export된 registerUser를 signupForm에서 호출하여 사용할 수 있게 됨
