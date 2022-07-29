53daac32b86d4edc4f211d8de8ac3b0b01c2fae2
seinJOO <94598267+seinJOO@users.noreply.github.com>
2022-07-29 10:40:48 +0900

    3-5 회원가입 폼 : axios를 instance로 공통 설정 담기(baseURL)

--- vue/vue-endgame/vue-til/src/api/index.js
+++ vue/vue-endgame/vue-til/src/api/index.js
@@ -1,8 +1,14 @@
 import axios from 'axios';
 
+const instance = axios.create({
+  baseURL: 'http://localhost:3000', // axios 인스턴스로 공통 설정을 담기
+});
+
 function registerUser(userData) {
-  const url = 'http://localhost:3000/signup';
-  return axios.post(url, userData);
+  return instance.post('signup', userData);
+
+  // const url = 'http://localhost:3000/signup';
+  //return axios.post(url, userData);
   //   (method) Axios.post (url: string, data?: any, config?: AxiosRequestConfig<any>)
   //    post()의 result는 Promise<AxiosResponse<any, any>>
 }
