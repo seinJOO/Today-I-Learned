724325e79fb139026b35dfdd0a67912b9e29e471
seinJOO <sein19950910@gmail.com>
2022-07-31 22:32:56 +0900

    5-3 로그인 상태관리 : 로그인 상태에 따른 분기처리
    store.getters에서 계산하는 로그인 상태 여부를 헤더로 가져와서, 메뉴버튼 표출여부 분기처리하기

--- vue/vue-endgame/vue-til/src/components/common/AppHeader.vue
+++ vue/vue-endgame/vue-til/src/components/common/AppHeader.vue
@@ -6,19 +6,36 @@
       </router-link>
     </div>
     <div class="navigations">
-      <!-- store에 저장된 state.username 데이터 꺼내기 -->
-      <span>{{ $store.state.username }}</span>
-      <router-link to="/login">로그인</router-link>
-      <router-link to="/signup">회원가입</router-link>
+      <!-- 분기처리 1 : 로그인이 되었을 때 표출 -->
+      <!-- <template v-if="$store.getters.isLogin">  
+            1) store의 getters에서 직접 가져오기-->
+      <template v-if="isUserLogin">
+        <!-- 2) computed를 통해 가져와서 코드를 더 간결하게 만들기 -->
+        <span class="username">{{ $store.state.username }}</span>
+      </template>
+      <!-- 분기처리 2 : 로그인이 되지 않았을 때 표출 -->
+      <template v-else>
+        <router-link to="/login">로그인</router-link>
+        <router-link to="/signup">회원가입</router-link>
+      </template>
     </div>
   </header>
 </template>
 
 <script>
-export default {};
+export default {
+  computed: {
+    isUserLogin() {
+      return this.$store.getters.isLogin; // this.를 통해 getters에 접근
+    },
+  },
+};
 </script>
 
 <style scoped>
+.username {
+  color: white;
+}
 header {
   display: flex;
   justify-content: space-between;
--- vue/vue-endgame/vue-til/src/store/index.js
+++ vue/vue-endgame/vue-til/src/store/index.js
@@ -7,6 +7,12 @@ export default new Vuex.Store({
   state: {
     username: '',
   },
+  getters: {
+    // getters : state값이 변경됐을 때 특정 상태를 계산 - computed와 비슷
+    isLogin(state) {
+      return state.username !== ''; // =*3 : 타입까지 비교
+    },
+  },
   mutations: {
     // state를 바꾸는 mutations - 새로운 username을 받아서 state의 값을 변경함
     setUsername(state, username) {
