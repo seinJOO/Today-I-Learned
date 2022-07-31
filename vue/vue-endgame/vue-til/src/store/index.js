d09fc958f8b2497fc3cd753d923eb0200bbdca71
seinJOO <sein19950910@gmail.com>
2022-08-01 08:34:25 +0900

    8-2 브라우저 저장소 : store의 state에 쿠키 저장값 가져오기

--- vue/vue-endgame/vue-til/src/store/index.js
+++ vue/vue-endgame/vue-til/src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import { getAuthFromCookie, getUserFromCookie } from '@/utils/cookies';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    username: '',
-    token: '',
+    // 쿠키값이 있으면 꺼내오고, 없으면 빈 문자열
+    username: getUserFromCookie() || '',
+    token: getAuthFromCookie() || '',
   },
   getters: {
     isLogin(state) {
