17664662d0268961ef1302d2b407cbf6a8e36040
seinJOO <sein19950910@gmail.com>
2022-07-31 21:13:31 +0900

    4-3 로그인 폼 : 유효성 검사

--- vue/vue-endgame/vue-til/.eslintrc.js
+++ vue/vue-endgame/vue-til/.eslintrc.js
@@ -1,36 +1,37 @@
+/* eslint-disable prettier/prettier */
 module.exports = {
   root: true,
   env: {
     node: true,
   },
-  extends: ['plugin:vue/essential', '@vue/prettier'],
+  extends: ["plugin:vue/essential", "@vue/prettier"],
   rules: {
-    'no-console': 'off',
+    "no-console": "off",
     // "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     // "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-    'prettier/prettier': [
-      'error',
+    "prettier/prettier": [
+      "error",
       {
         singleQuote: true,
         semi: true,
         useTabs: false,
         tabWidth: 2,
-        trailingComma: 'all',
-        printWidth: 80,
+        trailingComma: "all",
+        printWidth: 100,
         bracketSpacing: true,
-        arrowParens: 'avoid',
-        endOfLine: 'auto',
+        arrowParens: "avoid",
+        endOfLine: "auto",
       },
     ],
   },
   parserOptions: {
-    parser: '@babel/eslint-parser',
+    parser: "@babel/eslint-parser",
   },
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
       ],
       env: {
         jest: true,
--- vue/vue-endgame/vue-til/src/components/LoginForm.vue
+++ vue/vue-endgame/vue-til/src/components/LoginForm.vue
@@ -8,13 +8,18 @@
       <label for="password"> PW </label>
       <input id="password" type="text" v-model="password" />
     </div>
-    <button type="submit">로그인</button>
+    <button :disabled="!isUsernameValid || !password" type="submit">
+      <!-- v-bind:disabled="함수" => 함수가 false일 때 disabled는 true-->
+      <!-- !password : v-model="password"에 데이터가 없을 경우 false -->
+      로그인
+    </button>
     <p>{{ logMessage }}</p>
   </form>
 </template>
 
 <script>
 import { loginUser } from '@/api/index';
+import { validateEmail } from '@/utils/validation';
 
 export default {
   data() {
@@ -24,24 +29,25 @@ export default {
       logMessage: '',
     };
   },
+  computed: {
+    //props든, data든, store든 데이터의 변화에 따라서 내용을 자동으로 계산해주는 연산식!
+    isUsernameValid() {
+      // boolean을 반환해올것이기 때문에 is~~()
+      return validateEmail(this.username);
+      // username의 데이터가 한글자한글자 바뀔때마다 validateEmail()이 실행될 것
+    },
+  },
   methods: {
     async submitForm() {
       try {
-        // 비즈니스 로직
         const userData = {
           username: this.username,
           password: this.password,
         };
-        //const response = await loginUser(userData);
-        //console.log(response);
-        // 콘솔에 출력 시 response는 정해진 형식으로 수신됨
-
-        // data 부분에 우리가 원하는 정보가 있음 => response의 data를 바로 꺼내기 {data}
         const { data } = await loginUser(userData);
         console.log(data.user.username);
         this.logMessage = `${data.user.username}님 환영합니다`;
       } catch (error) {
-        // 에러 핸들링할 코드
         console.log(error); //=> try-catch를 통해 어떤 부분에서 에러가 났는지 콘솔에서 확인 가능
         console.log(error.response); // 에러에 대한 response를 볼 수 있음.
         console.log(error.response.data); // response의 data부분만 출력 - Authentication failed. Wrong password.
