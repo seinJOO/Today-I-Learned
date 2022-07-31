351e6010f56b01a9a0ffcb2b3ca711820feae39b
seinJOO <sein19950910@gmail.com>
2022-08-01 08:30:42 +0900

    8-1 브라우저 저장소 : 쿠키로 토큰값, 유저명 저장

--- vue/vue-endgame/vue-til/src/components/LoginForm.vue
+++ vue/vue-endgame/vue-til/src/components/LoginForm.vue
@@ -15,11 +15,7 @@
           <label for="password">pw:</label>
           <input id="password" type="text" v-model="password" />
         </div>
-        <button
-          :disabled="!isUsernameValid || !password"
-          type="submit"
-          class="btn"
-        >
+        <button :disabled="!isUsernameValid || !password" type="submit" class="btn">
           로그인
         </button>
       </form>
@@ -31,6 +27,7 @@
 <script>
 import { loginUser } from '@/api/index';
 import { validateEmail } from '@/utils/validation';
+import { saveAuthToCookie, saveUserToCookie } from '@/utils/cookies';
 
 export default {
   data() {
@@ -56,9 +53,12 @@ export default {
           password: this.password,
         };
         const { data } = await loginUser(userData);
-        console.log(data.token);
         this.$store.commit('setToken', data.token);
         this.$store.commit('setUsername', data.user.username);
+        console.log(data.user.username);
+        // 쿠키에 유저정보 저장하기
+        saveAuthToCookie(data.token); // 쿠키에 til_auth=토큰값 저장
+        saveUserToCookie(data.user.username); // 쿠키에 til_user=유저명 저장
         this.$router.push('/main');
       } catch (error) {
         // 에러 핸들링할 코드
--- vue/vue-endgame/vue-til/src/utils/cookies.js
+++ vue/vue-endgame/vue-til/src/utils/cookies.js
@@ -7,27 +7,15 @@ function saveUserToCookie(value) {
 }
 
 function getAuthFromCookie() {
-  return document.cookie.replace(
-    /(?:(?:^|.*;\s*)til_auth\s*=\s*([^;]*).*$)|^.*$/,
-    '$1',
-  );
+  return document.cookie.replace(/(?:(?:^|.*;\s*)til_auth\s*=\s*([^;]*).*$)|^.*$/, '$1');
 }
 
 function getUserFromCookie() {
-  return document.cookie.replace(
-    /(?:(?:^|.*;\s*)til_user\s*=\s*([^;]*).*$)|^.*$/,
-    '$1',
-  );
+  return document.cookie.replace(/(?:(?:^|.*;\s*)til_user\s*=\s*([^;]*).*$)|^.*$/, '$1');
 }
 
 function deleteCookie(value) {
   document.cookie = `${value}=; expires=Thu, 01 Jan 1970 00:00:01 GMT;`;
 }
 
-export {
-  saveAuthToCookie,
-  saveUserToCookie,
-  getAuthFromCookie,
-  getUserFromCookie,
-  deleteCookie,
-};
+export { saveAuthToCookie, saveUserToCookie, getAuthFromCookie, getUserFromCookie, deleteCookie };
