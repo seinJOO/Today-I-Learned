18a3db10926f37287253c5b5453e9208ff1587bc
seinJOO <sein19950910@gmail.com>
2022-08-01 08:16:14 +0900

    7-2 학습노트 조회 : 로딩화면 설정
    await 전후의 boolean값 설정 및 v-if를 이용한 로딩스피너 화면 설정

--- vue/vue-endgame/vue-til/src/views/MainPage.vue
+++ vue/vue-endgame/vue-til/src/views/MainPage.vue
@@ -2,10 +2,8 @@
   <div>
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
-      <ul>
-        <!-- v-for에서 key는 필수 !! -->
-        <!-- PostListItem.vue에서 props명을 postItem으로 지정했기 때문에 :postItem = "postItem" (:프롭스명="메인페이지에서의 변수명")
-        data.posts로 받은 postItems의 각 데이터를 postItem에 넣어 for문 돌림 -->
+      <LoadingSpinner v-if="isLoading"></LoadingSpinner>
+      <ul v-else>
         <PostListItem v-for="postItem in postItems" :key="postItem._id" :postItem="postItem">
           <!-- 이 부분이 PostListItem.vue로 컴포넌트화 된 것 !!!!
           <li><div class="post-title">{{ postItem.title }}</div>
@@ -19,22 +17,26 @@
 
 <script>
 import PostListItem from '@/components/posts/PostListItem.vue'; // 리스트를 출력하는 컴포넌트 가져오기
+import LoadingSpinner from '@/components/common/LoadingSpinner.vue'; // 로딩스피너 컴포넌트 가져오기
 import { fetchPosts } from '@/api/index';
 
 export default {
   components: {
     PostListItem, // 리스트를 출력하는 컴포넌트 등록
+    LoadingSpinner, // 로딩스피너를 컴포넌트로 등록
   },
   data() {
     return {
       postItems: [], // 콘솔에 data 출력 결과 posts:[]가 확인되어, 초기화는 []로 진행
+      isLoading: false,
     };
   },
   methods: {
     async fetchData() {
       // 학습 노트 데이터 조회 API를 메인페이지와 연결
+      this.isLoading = true; // 데이터를 가져올 때까지 로딩스피너 표출을 위해 isLoading=true 설정
       const { data } = await fetchPosts(); // axios의 response의 data 바로 꺼내오기
-      console.log(data.posts); // 콘솔에 찍었을 때 data.post에 우리가 원하는 값이 있음을 알 수 있음
+      this.isLoading = false;
       this.postItems = data.posts;
     },
   },
