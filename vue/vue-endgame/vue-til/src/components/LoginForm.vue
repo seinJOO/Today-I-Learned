85aaf0c1c9edc6a1dd214ac3738ccbfd877daf07
seinJOO <sein19950910@gmail.com>
2022-07-31 20:37:01 +0900

    4-2 로그인 구현 : 에러 처리
    try-catch, error.response.data ....

--- vue/vue-endgame/vue-til/src/components/LoginForm.vue
+++ vue/vue-endgame/vue-til/src/components/LoginForm.vue
@@ -26,19 +26,29 @@ export default {
   },
   methods: {
     async submitForm() {
-      const userData = {
-        username: this.username,
-        password: this.password,
-      };
-      //const response = await loginUser(userData);
-      //console.log(response);
-      // 콘솔에 출력 시 response는 정해진 형식으로 수신됨
+      try {
+        // 비즈니스 로직
+        const userData = {
+          username: this.username,
+          password: this.password,
+        };
+        //const response = await loginUser(userData);
+        //console.log(response);
+        // 콘솔에 출력 시 response는 정해진 형식으로 수신됨
 
-      // data 부분에 우리가 원하는 정보가 있음 => response의 data를 바로 꺼내기 {data}
-      const { data } = await loginUser(userData);
-      console.log(data.user.username);
-      this.logMessage = `${data.user.username}님 환영합니다`;
-      this.initForm();
+        // data 부분에 우리가 원하는 정보가 있음 => response의 data를 바로 꺼내기 {data}
+        const { data } = await loginUser(userData);
+        console.log(data.user.username);
+        this.logMessage = `${data.user.username}님 환영합니다`;
+      } catch (error) {
+        // 에러 핸들링할 코드
+        console.log(error); //=> try-catch를 통해 어떤 부분에서 에러가 났는지 콘솔에서 확인 가능
+        console.log(error.response); // 에러에 대한 response를 볼 수 있음.
+        console.log(error.response.data); // response의 data부분만 출력 - Authentication failed. Wrong password.
+        this.logMessage = error.response.data; // data의 에러 메세지를 화면/모달 등에 표출 가능
+      } finally {
+        this.initForm();
+      }
     },
     initForm() {
       this.username = '';
