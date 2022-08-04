55895147d558d0f4fde49b637e680fdf459821fc
seinJOO <94598267+seinJOO@users.noreply.github.com>
2022-08-04 11:14:44 +0900

    15 테스트 코드 : vue/test-utils를 활용해 쉽게 마운팅하여 테스트
    LoginForm 컴포넌트가 인스턴스로 마운팅 된 후 username이 존재하는지, 초기화가 잘 되고 있는지 vue/test-utils를 활용해 테스트코드로 확인하기

--- vue/vue-endgame/vue-til/src/components/LoginForm.spec.js
+++ vue/vue-endgame/vue-til/src/components/LoginForm.spec.js
@@ -1,16 +1,12 @@
-import Vue from 'vue';
+import { shallowMount } from '@vue/test-utils';
+//import Vue from 'vue';
 import LoginForm from './LoginForm.vue';
-// 컴포넌트 테스트
+// 컴포넌트 테스트 + vue/test-utils를 활용해보기
 // 테스트 단위를 LoginForm.vue로 grouping
 describe('LoginForm.vue', () => {
   test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정되어 있어야 한다.', () => {
-    /*
-    new Vue({
-      // el: '@app', // instance가 생성될 때 element를 지정하는 방법 - X
-      render: h => h(App); }).$mount(); // instance가 생성된 후 mount 될 포인트를 지정해주는 방법
-      */
-    const instance = new Vue(LoginForm).$mount();
-    console.log(instance.username); // jest에서는 콘솔을 터미널에서 편하게 확인할 수 있음
-    expect(instance.username).toBe(''); // username이 초기값(빈 문자열)이 맞는지 확인
+    const wrapper = shallowMount(LoginForm); // const instance = new Vue(LoginForm).$mount();
+    // wrapper.vm.username === instance.username (vm이 wrapper(shallowMount)내부의 인스턴스를 의미함)
+    expect(wrapper.vm.username).toBe(''); // expect(instance.username).toBe('');
   });
 });
