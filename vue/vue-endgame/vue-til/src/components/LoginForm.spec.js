04d1b1514dc87dd2c87bab4ed8a71150a44c1181
seinJOO <94598267+seinJOO@users.noreply.github.com>
2022-08-04 11:31:13 +0900

    15 테스트 코드 : 테스트 코드 내에서 마운팅 옵션을 변경하기
    data()를 설정하여 테스트 코드 내에서 실제 LoginForm.vue의 v-model 값을 변경시키기

--- vue/vue-endgame/vue-til/src/components/LoginForm.spec.js
+++ vue/vue-endgame/vue-til/src/components/LoginForm.spec.js
@@ -4,9 +4,16 @@ import LoginForm from './LoginForm.vue';
 
 describe('LoginForm.vue', () => {
   test('ID는 이메일 형식이어야 한다', () => {
-    const wrapper = shallowMount(LoginForm);
+    // shallowMount(component, options) 의 options : 컴포넌트를 마운팅 할 때의 option
+    const wrapper = shallowMount(LoginForm, {
+      // 실제로 컴포넌트를 마운팅 할 때 처럼 속성 설정 가능
+      data() {
+        return {
+          username: 'test', // 테스트코드 내에서 로그인폼에 정의된 v-model의 username 값을 바꾸게 됨
+        };
+      },
+    });
     const idInput = wrapper.find('#username'); // 로그인폼의 id="username" 값 추적
-    console.log(idInput); // Wrapper{}
-    console.log(idInput.html()); // <input id="username" type="text">
+    console.log(idInput.element.value); // test
   });
 });
