8dc95ccc7b5a955aa9769d751001f906278f5b29
seinJOO <94598267+seinJOO@users.noreply.github.com>
2022-08-04 11:48:41 +0900

    15 테스트 코드 : computed를 끌어와 유효성 검사 테스트하기

--- vue/vue-endgame/vue-til/src/components/LoginForm.spec.js
+++ vue/vue-endgame/vue-til/src/components/LoginForm.spec.js
@@ -2,6 +2,8 @@ import { shallowMount } from '@vue/test-utils';
 
 import LoginForm from './LoginForm.vue';
 
+// 실제 브라우저에서 확인하지 않아도 원하는 값이나 기능들이 동작하는지 확인하기
+
 describe('LoginForm.vue', () => {
   test('ID는 이메일 형식이어야 한다', () => {
     // shallowMount(component, options) 의 options : 컴포넌트를 마운팅 할 때의 option
@@ -14,6 +16,8 @@ describe('LoginForm.vue', () => {
       },
     });
     const idInput = wrapper.find('#username'); // 로그인폼의 id="username" 값 추적
-    console.log(idInput.element.value); // test
+    console.log('인풋 박스의 값', idInput.element.value); // test
+    // LoginForm의 이메일 유효성을 검증하는 computed 메서드를 끌어와 조건에 만족하는지 확인
+    console.log(wrapper.vm.isUsernameValid); // false
   });
 });
