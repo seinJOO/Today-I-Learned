03a8d83e735b4ef6a7aabb6f6f6bc24088c5a9ab
seinJOO <94598267+seinJOO@users.noreply.github.com>
2022-08-04 11:07:24 +0900

    15 테스트 코드 : 컴포넌트 마운팅 후 렌더링 확인
    LoginForm 컴포넌트가 인스턴스로 마운팅 된 후 username이 존재하는지, 초기화가 잘 되고 있는지 테스트코드로 확인하기

--- vue/vue-endgame/vue-til/src/components/LoginForm.spec.js
+++ vue/vue-endgame/vue-til/src/components/LoginForm.spec.js
@@ -1,23 +1,16 @@
-/*
-jest : 자바스크립트의 라이브러리 ! https://jestjs.io/docs/api#describename-fn
-테스트코드가 필요한 이유
-1. id 인풋박스에 이메일을 입력했을 때 이메일이 맞는지 확인하는 로직
-2. id, pw가 맞는 경우 로그인 처리가 되고, 다음 페이지로 이동
-=> console.log나 logMessage 등의 코드를 섞어가며 기능이 잘 작동하는지 확인해야 함
-BUT, 테스트코드로 하면 일일이 기능을 손으로 확인하는 시간을 줄여줌
-*/
-
-import { sum } from './math.js';
-
-// jest가 제공하는 API의 활용법
-
-// describe(name, fn) : 연관된 테스트 케이스를 그룹화하는 API
-describe('math.js', () => {
-  // test(name, fn, timeout) : 하나의 테스트 케이스를 검증하는 API
-  test('10 + 20 = 30', () => {
-    const result = sum(10, 20);
-    // expect(결과값).toBe(추정값); : 결과값이 추정값이라고 예상한다~ 를 설정
-    // result === 30; 과 같은 의미
-    expect(result).not.toBe(20); // not으로도 판별 가능
+import Vue from 'vue';
+import LoginForm from './LoginForm.vue';
+// 컴포넌트 테스트
+// 테스트 단위를 LoginForm.vue로 grouping
+describe('LoginForm.vue', () => {
+  test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정되어 있어야 한다.', () => {
+    /*
+    new Vue({
+      // el: '@app', // instance가 생성될 때 element를 지정하는 방법 - X
+      render: h => h(App); }).$mount(); // instance가 생성된 후 mount 될 포인트를 지정해주는 방법
+      */
+    const instance = new Vue(LoginForm).$mount();
+    console.log(instance.username); // jest에서는 콘솔을 터미널에서 편하게 확인할 수 있음
+    expect(instance.username).toBe(''); // username이 초기값(빈 문자열)이 맞는지 확인
   });
 });
