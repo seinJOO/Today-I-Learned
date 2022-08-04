ea773bb1cd43a53adc7d01b2964b5f3470b6e242
seinJOO <94598267+seinJOO@users.noreply.github.com>
2022-08-04 11:23:28 +0900

    15 테스트 코드 : @vue/test-utils의 find()를 이용한 테스트
    find()를 이용한 컴포넌트 요소 검색
    id를 이용한 추적 - #username
    추적 대상의 html 요소 검색 - __.html()

--- vue/vue-endgame/vue-til/src/components/LoginForm.spec.js
+++ vue/vue-endgame/vue-til/src/components/LoginForm.spec.js
@@ -1,12 +1,12 @@
 import { shallowMount } from '@vue/test-utils';
-//import Vue from 'vue';
+
 import LoginForm from './LoginForm.vue';
-// 컴포넌트 테스트 + vue/test-utils를 활용해보기
-// 테스트 단위를 LoginForm.vue로 grouping
+
 describe('LoginForm.vue', () => {
-  test('컴포넌트가 마운팅되면 username이 존재하고 초기 값으로 설정되어 있어야 한다.', () => {
-    const wrapper = shallowMount(LoginForm); // const instance = new Vue(LoginForm).$mount();
-    // wrapper.vm.username === instance.username (vm이 wrapper(shallowMount)내부의 인스턴스를 의미함)
-    expect(wrapper.vm.username).toBe(''); // expect(instance.username).toBe('');
+  test('ID는 이메일 형식이어야 한다', () => {
+    const wrapper = shallowMount(LoginForm);
+    const idInput = wrapper.find('#username'); // 로그인폼의 id="username" 값 추적
+    console.log(idInput); // Wrapper{}
+    console.log(idInput.html()); // <input id="username" type="text">
   });
 });
