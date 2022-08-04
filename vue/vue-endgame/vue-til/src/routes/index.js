224d99bb0edef3add6c1eb631b9e248f76c84a17
seinJOO <94598267+seinJOO@users.noreply.github.com>
2022-08-04 09:43:12 +0900

    14 라우터 심화 : 페이지별 인증 권한 설정 및 접근 제어
    콜백인자 to의 meta속성의 파라미터를 이용한 인증 권한 설정 및 접근 제어
    store/index.js의 로그인 여부 확인 메서드 활용

--- vue/vue-endgame/vue-til/src/routes/index.js
+++ vue/vue-endgame/vue-til/src/routes/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-// import LoginPage from '@/views/LoginPage.vue';
-// import SignupPage from '@/views/SignupPage.vue';
+import store from '@/store/index';
 
 Vue.use(VueRouter);
 
@@ -24,14 +23,18 @@ const router = new VueRouter({
     {
       path: '/main',
       component: () => import('@/views/MainPage.vue'),
+      // 임의로 정의한 속성 auth를 설정 - auth가 false이면 로그인 페이지로 이동하도록 구현
+      meta: { auth: true },
     },
     {
       path: '/add',
       component: () => import('@/views/PostAddPage.vue'),
+      meta: { auth: true },
     },
     {
       path: '/post/:id',
       component: () => import('@/views/PostEditPage.vue'),
+      meta: { auth: true },
     },
     {
       path: '*',
@@ -42,8 +45,16 @@ const router = new VueRouter({
 
 // 콜백 인자 - to : 이동하려는 페이지 / from : 현재 페이지 / next : 페이지 이동 API 호출
 router.beforeEach((to, from, next) => {
-  console.log(to);
-  next(); // next()를 호출해야만 다음 페이지로 이동할 수 있음 ***
+  // store의 index.js에 로그인 여부 확인 가능
+  // getters: {isLogin(state) {return state.username !== '';},},
+  if (to.meta.auth && !store.getters.isLogin) {
+    // to의 meta속성을 이용해 임의로 인증 설정해보기
+    console.log('인증이 필요합니다');
+    next('/login'); // 로그인 상태가 아닌 경우 로그인 페이지로 이동
+    return; // => if를 사용했기 때문에 페이지를 이동했을 경우 아래의 next()가 또 호출되지 않도록 return;처리
+  }
+  console.log(to); // to를 콘솔에 찍어보면 meta 속성이 있음
+  next(); // next()를 호출해야만 다음 페이지로 이동할 수 있음
 });
 
 export default router;
